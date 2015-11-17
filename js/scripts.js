b39eef8ca4361dfd8b67576b84eff9e0b0debd0c
MrMendonez <dmendonez@gmail.com>
2015-11-16 22:52:17 -0500

    got added rows to appear above the input fields instead of below!

--- js/scripts.js
+++ js/scripts.js
@@ -22,28 +22,26 @@ add.addEventListener("click", rowAdd);
 
 // Function to add row
 function rowAdd() {
-  console.log("in rowAdd");
   var firstName = document.getElementById("firstName").value;
   var lastName = document.getElementById("lastName").value;
   var email = document.getElementById("email").value;
   var table = document.getElementsByTagName("tbody")[0];
   var newRowEl = document.createElement("tr");
+  // add HTML for each new row
   newRowEl.setAttribute("class", "visible inflow");
   newRowEl.setAttribute("data-visible", "true");
   newRowEl.innerHTML = "<td>" + firstName + "</td> \
                   <td>" + lastName + "</td> \
                   <td>" + email + "</td> \
                   <td><button type='button' class='btn btn-danger \ removeButton'><i class='fa fa-times'></i></button></td>";
-
   table.appendChild(newRowEl);
-
-  setTimeout(function() {
-    add.addEventListener("click", rowAdd);  
-    console.log("added;");
-  }, 1500);
-  
+  // Set Eventlisteners to listen to new buttons from added rows
+  add.addEventListener("click", rowAdd);  
   for (i = 0; i < hide.length; i++) {
     hide[i].addEventListener("click", rowRemove); 
   };
+  // Reset form fields back to place-holders
+  var form = document.getElementById("form");
+  form.reset();
 };
 
