64b254e763865e32bcf5aecac9b6d29a2236f806
MrMendonez <dmendonez@gmail.com>
2015-11-16 20:40:32 -0500

    got added rows working

--- js/scripts.js
+++ js/scripts.js
@@ -22,5 +22,20 @@ add.addEventListener("click", rowAdd);
 
 // Function to add row
 function rowAdd() {
-  console.log('this');
+
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+  var table = document.getElementsByTagName("tbody")[0];
+  var newRow = "<tr class='visible infoRow' data-visible='true'> \
+                  <td>" + firstName + "</td> \
+                  <td>" + lastName + "</td> \
+                  <td>" + email + "</td> \
+                  <td><button type='button' class='btn btn-danger \ removeButton'><i class='fa fa-times'></i></button></td> \
+                </tr>"
+  table.innerHTML += newRow
+  for (i = 0; i < hide.length; i++) {
+    hide[i].addEventListener("click", rowRemove); 
+  };
 }
+
