2827a906e96ee8ff69c0d549f08af24764279b49
MrMendonez <dmendonez@gmail.com>
2015-11-16 21:23:38 -0500

    shamoon figured out how to add more than one row

--- js/scripts.js
+++ js/scripts.js
@@ -22,17 +22,26 @@ add.addEventListener("click", rowAdd);
 
 // Function to add row
 function rowAdd() {
+  console.log("in rowAdd");
   var firstName = document.getElementById("firstName").value;
   var lastName = document.getElementById("lastName").value;
   var email = document.getElementById("email").value;
   var table = document.getElementsByTagName("tbody")[0];
-  var newRow = "<tr class='visible infoRow' data-visible='true'> \
-                  <td>" + firstName + "</td> \
+  var newRowEl = document.createElement("tr");
+  newRowEl.setAttribute("class", "visible inflow");
+  newRowEl.setAttribute("data-visible", "true");
+  newRowEl.innerHTML = "<td>" + firstName + "</td> \
                   <td>" + lastName + "</td> \
                   <td>" + email + "</td> \
-                  <td><button type='button' class='btn btn-danger \ removeButton'><i class='fa fa-times'></i></button></td> \
-                </tr>"
-  table.innerHTML += newRow
+                  <td><button type='button' class='btn btn-danger \ removeButton'><i class='fa fa-times'></i></button></td>";
+
+  table.appendChild(newRowEl);
+
+  setTimeout(function() {
+    add.addEventListener("click", rowAdd);  
+    console.log("added;");
+  }, 1500);
+  
   for (i = 0; i < hide.length; i++) {
     hide[i].addEventListener("click", rowRemove); 
   };
